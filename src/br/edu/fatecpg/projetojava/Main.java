eb1fc12f90176dab663f9829d82bbfb09ee0e439
Leonardo <mendoncaleonardo547@gmail.com>
2025-02-21 15:29:16 -0300

    Função de Soma

--- src/br/edu/fatecpg/projetojava/Main.java
+++ src/br/edu/fatecpg/projetojava/Main.java
@@ -1,9 +1,17 @@
 package br.edu.fatecpg.projetojava;
-
+import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
-	System.out.println("Primeiro Controle de Versionamento Git");
+		Scanner scan = new Scanner(System.in);
+	System.out.println("Digite um valor");
+	int n1 = scan.nextInt();
+	
+	System.out.println("Digite um valor");
+	int n2 = scan.nextInt();
+	System.out.println("A soma é " + (n1 + n2));
+	
+	
 
 	}
 
